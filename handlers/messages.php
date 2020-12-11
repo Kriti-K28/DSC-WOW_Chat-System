172390b0b51b6a37b1b417bca3d63a0cd003bf2a
Kriti Kumari <1905243@kiit.ac.in>
2020-12-11 12:54:08 +0530

    second commit

---
+++ handlers/messages.php
@@ -0,0 +1,35 @@
+<?php
+   //we have to include config file of php
+   include('../config.php');
+   switch($_REQUEST['action'])
+   {
+       //sendMessage is a variable from jquery code in index.php file 
+       case "sendMessage":
+        //global $db;
+        session_start();
+        //pass query
+        $query=$db->prepare("INSERT INTO messages SET user=? ,message=?");
+      $run=  $query->execute([$_SESSION['username'],$_REQUEST['message']]);
+        
+        if ($run)
+        {
+            echo 1;
+            exit;
+        }
+       break;
+
+       case "getMessages":
+        // $query=$db->prepare("SELECT * FROM messages");
+        // $run=$query->execute();
+        // $rs=$query->fetchAll(PDO::FETCH_OBJ);
+        // echo var_dump($rs);
+        echo 'working';
+        // $chat='';
+        // foreach( $rs as $message)
+        // {
+        //   $chat .=$message->message;
+        // }
+        // echo $chat;
+      break;
+   }
+ ?>
---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>My Chat</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.js" 
+    integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+    <?php
+       session_start();
+        $_SESSION['username']="Abc Xyz";
+    ?>
+
+     <div id="wrapper">        
+        <div id="left_panel">
+           <div style=" padding: 10px;">
+              <img id="profile_imag" src="UI/imag/avatar.png" alt="">
+              <br>
+              Name:ABC
+              <br>
+              <span style="font-size: 12px; opacity: 0.5;">abc@gmail.com</span>
+              <br><br><br>
+              <div>
+                <label id="label_chat" for="radio_chat">Chat <img src="UI/imag/demo.jpg" alt=""></label>
+                <label id="label_contact" for="radio_contacts">Contacts <img src="UI/imag/demo.jpg" alt=""></label>
+                <label id="label_setting" for="radio_setting">Setting <img src="UI/imag/demo.jpg" alt=""></label>
+              </div> 
+           </div>
+        </div>
+        <div id="right_panel">
+           <div id="header">Let's Chat</div>
+           <div id="container" style="display: flex;">
+              <div id="inner_left_panel">
+              
+              </div>
+              <input type="radio" id="radio_chat" name="myradio" style="display: none;">
+              <input type="radio" id="radio_contacts" name="myradio" style="display: none;">
+              <input type="radio" id="radio_setting" name="myradio" style="display: none;">
+              <div id="inner_right_panel">
+                 <div class="chat"> 
+                     <p>hiii</p>
+                 </div>
+                <hr>
+                  <form action="" method="POST" id="messageForm">
+                 
+                    <textarea name="message"  class="textarea"id="" cols="50" rows="5"></textarea>
+                 </form>
+              </div>
+           </div>
+        </div>
+    </div>
+    <!--Jquery code for textarea to submit the message -->
+    <script>
+         LoadChat();
+        function LoadChat()
+        {
+            $.post('handler/messages.php?action=getMessages',function(response)
+            {
+                $('.chat').html(response);
+            });
+        }
+        $('.textarea').keyup(function(e)
+        {
+            //whenever user hit enter form submitted
+            if(e.which == 13)
+            {
+                $('form').submit();
+            } 
+        });
+        $('form').submit(function() 
+        {
+           var message = $('.textarea').val();
+           $.post('handlers/messages.php?action=sendMessage&message='+message,function(response)
+           {
+              if(response==1)
+              {
+                  document.getElementById('messageForm').reset();
+              }
+           });
+           return false;
+        });
+    </script> 
+</body>
+</html>
+<!-- JS code for animation of panel  -->
+<script type="text/javascript">
+    function _(element)
+    {
+       return document.getElementById(element);
+    }
+    var label=_("label_chat");
+    label.addEventListener("click",function(){
+      var inner_panel= _("inner_left_panel");
+      var ajax=new XMLHttpRequest();
+      ajax.onload=function()
+      {
+         if(ajax.status==200|| ajax.readyState==4)
+         {
+            inner_panel.innerHTML=ajax.responseText;
+         }
+      }
+      ajax.open("POST","file.php",true);
+      ajax.send();
+    });
+</script>
