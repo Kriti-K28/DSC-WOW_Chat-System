e0269aee65e8e398b1564b01598f901cd33ec9b7
Kriti Kumari <1905243@kiit.ac.in>
2020-12-12 21:15:30 +0530

    seventeenth commit

--- home.php
+++ home.php
@@ -28,19 +28,19 @@
               <span style="font-size: 12px; opacity: 0.5;">abc@gmail.com</span>
               <br><br><br>
               <div style="color:#30475e">
-                <label id="label_chat" for="radio_chat">Chat <img src="assets/images/chat.png" alt=""></label>
-                <label id="label_contact" for="radio_contacts">Contacts <img src="assets/images/contact.png" alt=""></label>
-                <label id="label_setting" for="radio_setting">Setting <img src="assets/images/setting.png" alt=""></label>
+                <label id="label_chat" for="radio_chat" >Chat <img src="assets/images/chat.png" alt="" style="margin:-7px 30px 0px -20px"></label>
+                <label id="label_contact" for="radio_contacts">Contacts <img src="assets/images/contact.png" alt=""style="margin:-7px 30px 0px -20px"></label>
+                <label id="label_setting" for="radio_setting">Setting <img src="assets/images/setting.png" alt="" style="margin:-7px 30px 0px -20px"></label>
                 <!-- Logout session start here -->
-                <div action="logout.php" class="popup" onclick="myFunction()">
-                      <input type="submit" class="button"  value="Logout" style="border-radius:5px; width:80px;margin-top:3px; height:25px; cursor:pointer;">
-                      <span class="popuptext" id="myPopup">Are you sure ?
+                <input type="submit" class="button"  value="Logout" onclick="myFunction()">
+                <div action="logout.php" class="popup" >     
+                      <span class="popuptext" id="myPopup">Are you sure? 
                       <form  method="POST" action="logout.php">  
                           <input type="submit"  class="button" value="yes" id="yes_btn">
                       </form> 
                        <input type="submit"  class="button" value="No" id="no_btn">
                     </span>
-               </div>
+                 </div>
                 <script>
                  // When the user clicks on div, open the popup
                     function myFunction() {
@@ -56,9 +56,23 @@
            
            <div id="container" style="display: flex;">
               <div id="inner_left_panel">
-                 <br>
-              <div style="background-color:black">Chat <br><br><br></div>
-              </div>
+                  <div class="contact_class">
+                       <img id="contact_img"  src="assets/images/user.png" alt="">
+                       <p id="contact_text">Contact 1 </p>
+                  </div>
+                  <div class="contact_class">
+                     <img id="contact_img" src="assets/images/user.png" src="assets/images/user.png" alt="">
+                     <p id="contact_text">Contact 2 </p>
+                  </div>
+                  <div class="contact_class">
+                     <img id="contact_img"src="assets/images/user.png" src="assets/images/user.png" alt="">
+                     <p id="contact_text">Contact 3 </p>
+                  </div>
+                  <div class="contact_class">
+                     <img id="contact_img" src="assets/images/user.png" src="assets/images/user.png" alt="">
+                     <p id="contact_text">Contact 3 </p>
+                  </div>
+               </div>   
               <input type="radio" id="radio_chat" name="myradio" style="display: none;">              
               <input type="radio" id="radio_contacts" name="myradio" style="display: none;">
               <input type="radio" id="radio_setting" name="myradio" style="display: none;">
@@ -87,9 +101,10 @@
                  </div>
                  
                          <form  method="POST" id="messageForm" onSubmit="window.location.reload()" action="send.php">  
-                         <textarea name="msg"  class="textarea" class="form-control" cols="50" rows="3" style="width:70% padding:0 0 0 20px; border-radius:10px; " ></textarea>
+                         <textarea name="msg"  class="textarea" class="form-control" cols="50" rows="3" style="width:70% padding:0 0 5px 20px; border-radius:10px; " ></textarea>
                       <br>
-                         <input type="submit"  id="send" class="button"  value="Send" > <br>             
+                         <input type="submit"  id="send" class="button"  value="Send" > 
+                         <br>             
                     </tr>
                  </table>
                 
